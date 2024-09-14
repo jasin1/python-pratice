d6e641c0417a0fdcfcdf49fda32b91f5e30aa2bb
jasin1 <jasin.tairaidrissi@gmail.com>
2024-09-14 14:21:34 +0200

    Continued Python Practice

---
+++ conditional-assignments.py
@@ -0,0 +1,15 @@
+#------------- first ----------------#
+
+# score = int(input('Enter a score between 0 & 100: '))
+
+# # conditional assignments / ternary
+
+# is_top_score = True if score >= 90 else False
+# print('is_top_score: ', is_top_score)
+
+# conditional assignments / ternary
+
+temp = int(input('Enter a temp in celsius between 0 & 40: '))
+
+weather = "hot" if temp > 25 else ("mild" if temp > 15 else "cold")
+print('The weather is ', weather)
---
+++ while-loops.py
@@ -0,0 +1,13 @@
+# While loops
+
+score = 0
+
+# while score < 10:
+#   print('The score is: ', score)
+#   score +=1
+  
+user_input = ""
+
+while user_input != "q":
+  user_input = input("Enter a letter or enter 'q' to quit: ")
+  print('You entered: ', user_input)
